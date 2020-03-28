d7441777c81e2cf221a8c00ee4fbb3c1cfadf6fc
ali-gungor <aligng1620@gmail.com>
2020-03-28 16:39:43 +0300

    Initial commit

---
+++ Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CommunicaptionBackend.Controllers {
+
+    [Route("/")]
+    public class TestController : ControllerBase {
+
+        [HttpGet("deneme")]
+        public IActionResult Deneme() {
+            return ActionResults.Json(new {
+                message = "deneme"
+            });
+        }
+    }
+}
