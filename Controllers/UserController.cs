958bcb037853ef9ca618ff56e3a7ba80f2222dfc
SemihCatal <catalsemih03@gmail.com>
2020-03-28 21:14:54 +0300

    UserController created and generatePın method implented.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommunicaptionBackend.Controllers
+{
+    [Route("/")]
+    public class UserController : Controller
+    {
+        private Guid guidPin;
+
+        [HttpGet("pin")]
+        public ActionResult GeneratePin()
+        {
+            guidPin = Guid.NewGuid();
+            return Content(guidPin.ToString());
+        }
+    }
+}
