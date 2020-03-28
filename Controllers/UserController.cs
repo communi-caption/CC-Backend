197ed3ce6159693adf066a5136c5b61cbecabe8e
SemihCatal <catalsemih03@gmail.com>
2020-03-28 22:04:25 +0300

    Added GetMediaRequest wrapper and getmedia request.

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using CommunicaptionBackend.Wrappers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommunicaptionBackend.Controllers
@@ -12,10 +17,30 @@ namespace CommunicaptionBackend.Controllers
         private Guid guidPin;
 
         [HttpGet("pin")]
-        public ActionResult GeneratePin()
+        public IActionResult GeneratePin()
         {
             guidPin = Guid.NewGuid();
             return Content(guidPin.ToString());
+        }
+
+        [HttpGet("media/{userId}/{mediaId}")]
+        public HttpResponseMessage GetMedia(string userId, string mediaId)
+        {
+            var stream = new MemoryStream();
+
+            var result = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new ByteArrayContent(stream.GetBuffer())
+            };
+            result.Content.Headers.ContentDisposition = new GetMediaRequest
+            {
+                UserId = userId,
+                MediaId = mediaId
+ 
+            };
+            result.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+            return result;
         }
     }
 }
---
+++ Wrappers/GetMediaRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace CommunicaptionBackend.Wrappers
+{
+    public class GetMediaRequest
+    {
+        public string UserId { get; set; }
+        public string MediaId { get; set; }
+
+        public static implicit operator ContentDispositionHeaderValue(GetMediaRequest v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
