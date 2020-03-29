074fedc970f9d90461a780db59d1183af3a5a040
dilaradogan <dilaradogan097@gmail.com>
2020-03-29 13:36:18 +0300

    Message.cs and MessageTypes.cs added.

---
+++ Wrappers/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommunicaptionBackend.Wrappers
+{
+    public class Message
+    {
+        public MessageTypes messageTypes;
+        public Media media;
+        public Settings settings;
+
+    }
+}
---
+++ Wrappers/MessageTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommunicaptionBackend.Wrappers
+{
+    public enum MessageTypes
+    {
+        SettingsChanged,
+        SavePhoto,
+        SaveVideo
+    };
+}
